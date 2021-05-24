5225315e2d5580e0b7da76baebdc75e7903b2b8f
jowc <jchikeme@gmail.com>
2021-05-24 12:58:41 +0100

    Added heroes component and passed props

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from '../mock-heroes';
 
@@ -12,7 +12,6 @@ export class HeroesComponent implements OnInit {
     id : 1,
     name: "Williams"
   }
-  
   heroes = HEROES
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
